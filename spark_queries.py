ae5ea7f4fc2f972fe1f337a8ba9f82635a4c5495
Fernando Gianini <fernandogianini@gmail.com>
2022-07-17 13:58:44 -0300

    feat(PySpark): tests each table from db
    Tests each table from our Database project. Currently only testing if Spark can get structure.
    It is still needed to work on our ETL.

--- spark_queries.py
+++ spark_queries.py
@@ -16,7 +16,17 @@ spark = SparkSession \
 
 print('----> [SPARK] Iniciando sessão OK!\n')
 
-prices_df = spark.read.format("jdbc") \
+print('----> [SPARK] Obtendo as tabelas via PostgreSQL\n')
+
+imports_sdf = spark.read.format("jdbc") \
+        .options(
+                url='jdbc:postgresql://localhost:5432/pad_constellation',
+                dbtable='fimportacoes',
+                user='postgres',
+                driver='org.postgresql.Driver') \
+        .load()
+
+prices_sdf = spark.read.format("jdbc") \
         .options(
                 url='jdbc:postgresql://localhost:5432/pad_constellation',
                 dbtable='fprecos',
@@ -24,4 +34,53 @@ prices_df = spark.read.format("jdbc") \
                 driver='org.postgresql.Driver') \
         .load()
 
-prices_df.printSchema()
+date_sdf = spark.read.format("jdbc") \
+        .options(
+                url='jdbc:postgresql://localhost:5432/pad_constellation',
+                dbtable='ddata',
+                user='postgres',
+                driver='org.postgresql.Driver') \
+        .load()
+
+products_sdf = spark.read.format("jdbc") \
+        .options(
+                url='jdbc:postgresql://localhost:5432/pad_constellation',
+                dbtable='dproduto',
+                user='postgres',
+                driver='org.postgresql.Driver') \
+        .load()
+
+state_sdf = spark.read.format("jdbc") \
+        .options(
+                url='jdbc:postgresql://localhost:5432/pad_constellation',
+                dbtable='duf',
+                user='postgres',
+                driver='org.postgresql.Driver') \
+        .load()
+
+country_sdf = spark.read.format("jdbc") \
+        .options(
+                url='jdbc:postgresql://localhost:5432/pad_constellation',
+                dbtable='dpais',
+                user='postgres',
+                driver='org.postgresql.Driver') \
+        .load()
+
+print('----> [SPARK] Obtendo as tabelas via PostgreSQL OK!\n')
+
+print('----> [SPARK] Testando as tabelas obtidas\n')
+
+print('FImportacoes:')
+imports_sdf.printSchema()
+print('FPrecos:')
+prices_sdf.printSchema()
+print('DData:')
+date_sdf.printSchema()
+print('DProduto:')
+products_sdf.printSchema()
+print('DUF:')
+state_sdf.printSchema()
+print('DPais:')
+country_sdf.printSchema()
+
+print('----> [SPARK] Testando as tabelas obtidas OK!\n')
