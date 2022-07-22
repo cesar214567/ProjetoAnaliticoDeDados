f1057dbf67deb24fd9c4ed260e572d907cc0f1d6
Fernando Gianini <fernandogianini@gmail.com>
2022-07-21 22:08:51 -0300

    fix(queries): fixes where clauses

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -12,25 +12,10 @@ class Extractor:
     self.precos_df = pd \
       .read_csv(path.join(c.base_csv_data_path, c.prices_filename), sep='\t')
 
-    self.usd_brl_df = pd \
-      .read_csv(path.join(c.base_csv_data_path, c.historical_usd_brl), sep='\t')
-
     print('----> Extraindo dados OK!\n')
-
-  # def get_gas_df(self):
-  #   return self.gas_df
-
-  # def get_derivados_df(self):
-  #   return self.derivados_df
-
-  # def get_etanol_df(self):
-  #   return self.etanol_df
 
   def get_importacoes_df(self):
     return self.importacoes_df
 
   def get_precos_df(self):
     return self.precos_df
-
-  def get_usd_brl_df(self):
-    return self.usd_brl_df
